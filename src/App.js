e7af2363724b18a77c97b925814a8def129a9abd
Lokesh Kumar <lokeshkumardubai1990@gmail.com>
2022-05-01 14:26:45 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,10 +2,26 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userList: [],
+    };
+  }
+  /*mounting component before render method*/
+
+  componentWillMount() {
+    alert("Component will mount called");
+  }
+
+  componentDidMount() {
+    alert("after mounting will call componentdidmount");
+  }
   render() {
     return (
       <div className="App">
         <h1>Welcom to react learning</h1>
+        {alert("Render method called")}
       </div>
     );
   }
