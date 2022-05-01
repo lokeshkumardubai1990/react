4d86ad9c989c84012aae77bf515a6ad8f204070b
Lokesh Kumar <lokeshkumardubai1990@gmail.com>
2022-05-01 14:19:13 +0530

    life cycle first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcom to react learning</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
