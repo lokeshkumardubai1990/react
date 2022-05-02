fb29919b28af3b779f33fc52c4c6149be3f3a9a8
Lokesh Kumar <lokeshkumardubai1990@gmail.com>
2022-05-02 09:53:25 +0530

    mounting lifecycle updated

--- src/App.js
+++ src/App.js
@@ -1,71 +1,34 @@
 import { toHaveAccessibleDescription } from "@testing-library/jest-dom/dist/matchers";
 import React from "react";
 import "./App.css";
-import Home from "./Home";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      userName: "John",
-      showHome: true,
+      name: "John",
     };
-    console.log("Constructor");
+    console.log("Constructor Triggered", this.state.name);
+    var ab = "vivek";
   }
+
+  /*before rendering*/
 
   componentWillMount() {
-    console.log("component will mount");
+    const ab = "test ab";
+    this.setState({ name: ab });
+    console.log("ComponentWillMount Triggered", this.state.name);
   }
+
+  /*after dome render*/
 
   componentDidMount() {
-    console.log("component did mount");
+    this.setState({ name: "Lokesh" });
+    console.log("ComponentDidMount Triggered", this.state.name);
   }
-
-  componentWillReceiveProps(nextProps) {
-    console.log("component will receive props", nextProps);
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log("shouldcomponent update", nextProps, nextState);
-    return true;
-  }
-
-  componentWillUpdate() {
-    console.log("componentwill update");
-  }
-
-  componentDidUpdate() {
-    console.log("component did update");
-  }
-  componentWillUnmount() {
-    console.log("component will unmount");
-  }
-
-  changeUserName = () => {
-    this.setState({ userName: "Peter" });
-  };
-
-  removeHome = () => {
-    this.setState({ showHome: !this.state.showHome });
-  };
 
   render() {
-    let showHH = "";
-    if (this.state.showHome) {
-      showHH = (
-        <Home
-          changeUserName={() => this.changeUserName}
-          removeHome={() => this.removeHome}
-        />
-      );
-    }
-    return (
-      <div className="App">
-        <h1>Welcom {this.state.userName}</h1>
-        {showHH}
-        <button onClick={this.removeHome}>Show & Remove Home component</button>
-      </div>
-    );
+    return <div className="App">{this.state.name}</div>;
   }
 }
 
